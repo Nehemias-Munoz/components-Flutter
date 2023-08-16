9ea5ecaacb8ca7d0da563c901f1ba3fb1d8a479b
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-08-16 18:20:51 -0400

    Add: nuevas pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/src/screens/listview1_screen.dart';
+import 'package:flutter_components/src/screens/screens.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ListView1Screen(),
+    return MaterialApp(
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+        "listview": (BuildContext context) => const ListView1Screen(),
+        "alert": (context) => const AlertScreen(),
+        "card": (context) => const CardScreen(),
+      },
     );
   }
 }
---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_components/src/screens/home_screen.dart';
+export 'package:flutter_components/src/screens/card_screen.dart';
+export 'package:flutter_components/src/screens/listview1_screen.dart';
+export 'package:flutter_components/src/screens/alert_screen.dart';
