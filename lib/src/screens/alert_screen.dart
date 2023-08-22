0531f2ce8f059d7fb91982d1d33be080c8780f35
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-08-22 17:09:53 -0400

    Add: tarjeta alerta

--- lib/src/screens/alert_screen.dart
+++ lib/src/screens/alert_screen.dart
@@ -3,11 +3,47 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancelar")),
+          FilledButton(onPressed: () {}, child: const Text("Aceptar"))
+        ],
+        title: const Text("Alerta 1"),
+        elevation: .5,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Contenido de la alerta"),
+            SizedBox(
+              height: 10,
+            ),
+            FlutterLogo(
+              size: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          onPressed: () => displayDialog(context),
+          child: const Text('Mostrar alerta'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pop(context),
+        child: const Icon(Icons.close),
       ),
     );
   }
--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -2,17 +2,28 @@ import 'package:flutter/material.dart';
 
 //Archivo de temas de la app
 class AppTheme {
-  static const Color lightPrimaryColor = Colors.grey;
+  static const Color lightPrimaryColor = Colors.indigo;
   static const Color darkPrimaryColor = Colors.indigo;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: lightPrimaryColor,
-    appBarTheme: const AppBarTheme(color: lightPrimaryColor, elevation: 0.5),
+      primaryColor: lightPrimaryColor,
+      appBarTheme: const AppBarTheme(color: lightPrimaryColor, elevation: 0.5),
 
-    //Textbutton Theme
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: Colors.indigo),
-    ),
-  );
+      //Textbutton Theme
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: Colors.indigo),
+      ),
+      //Floating action button
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: lightPrimaryColor,
+        elevation: 0.5,
+      ),
+      //Elevate button
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.amber,
+          shape: StadiumBorder(),
+        ),
+      ));
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: darkPrimaryColor,
   );
