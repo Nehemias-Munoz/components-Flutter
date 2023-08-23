3e07dec478b7692fa808e6876867c3d1dc3bd7a3
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-08-23 12:59:39 -0400

    Add: avatar grande/pequeno

--- lib/src/screens/avatar_screen.dart
+++ lib/src/screens/avatar_screen.dart
@@ -2,12 +2,28 @@ import 'package:flutter/material.dart';
 
 class AvatarScreen extends StatelessWidget {
   const AvatarScreen({Key? key}) : super(key: key);
-
+  //
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Avatar Screen"),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.indigo[900],
+              child: const Text("SL"),
+            ),
+          ),
+        ],
+      ),
       body: Center(
-         child: Text('AvatarScreen'),
+        child: CircleAvatar(
+          maxRadius: 100,
+          backgroundImage: NetworkImage(
+              "https://31minutosoficial.cl/wp-content/uploads/2014/02/thumb-rombosman.png"),
+        ),
       ),
     );
   }
